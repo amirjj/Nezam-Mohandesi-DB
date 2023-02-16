b1a2b58e3fea463780fa0c09c2a689f87743c22f
amirjj <amir.jamshidijam@gmail.com>
2023-02-16 21:23:21 +0330

    parser added

---
+++ parser_app/parser.py
@@ -0,0 +1,13 @@
+from parser_config import output_path
+
+from bs4 import BeautifulSoup
+
+
+def crawl():
+    for file in output_path.glob('*.html'):
+
+
+
+if __name__ == "__main__":
+    crawl()
+
---
+++ parser_app/parser_config.py
@@ -0,0 +1,10 @@
+from pathlib import Path
+
+
+table_xpath = '//*[@id="GridView1"]'
+
+
+
+root_path = Path(__file__).parent.parent
+output_path = root_path / 'output' / 'newbackup'
+
